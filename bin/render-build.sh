dfe3668972c13bb630436895b4b5e70f74016125
pmnoos <petermagner3@gmail.com>
2025-06-22 13:02:05 +1000

    Fix build script permissions and update PostgreSQL gem version

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,7 +2,12 @@
 # exit on error
 set -o errexit
 
+# Install dependencies
 bundle install
+
+# Precompile assets
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
+
+# Run database migrations
 bundle exec rake db:migrate 
